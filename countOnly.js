1d411bee1c968c35bee2b7fbd97dbd01fbb2fd3b
PeterDevelops <PeterDinhBC@gmail.com>
2024-01-29 13:54:10 -0800

    Completed countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+// Compares 2 value 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) { // truthy value
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+    }
+  }
+}
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+// result1 is the result of the countOnly function which should be an object {Jason: 1, Fang: 2};
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
