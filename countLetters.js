a22025bb6e67032f96c5579a962c42cd642c540c
PeterDevelops <PeterDinhBC@gmail.com>
2024-01-29 15:12:41 -0800

    Added more reliable test cases

--- countLetters.js
+++ countLetters.js
@@ -27,6 +27,10 @@ const countLetters = function(sentence) {
 // console.log(countLetters("lighthouse in the house"));
 // console.log(countLetters("LHL"));
 // console.log(countLetters("lighthouse"));
+
+const result = countLetters("lighthouse in the house");
+assertEqual(result["l"], 1); // true 
+assertEqual(result["h"], 4); // true
 assertEqual(JSON.stringify(countLetters("lighthouse in the house")), JSON.stringify({l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1,})); // pass
 assertEqual(JSON.stringify(countLetters("lighthouse")), JSON.stringify({l: 1, i: 1, g: 1, h: 2, t: 1, o: 1, u: 1, s: 1, e: 1})); // pass
 assertEqual(JSON.stringify(countLetters("LHL")), JSON.stringify({L: 2, H: 1})); // pass
