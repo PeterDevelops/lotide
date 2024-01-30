bde45a8b72367194862575a2f6116b56c7bbec92
PeterDevelops <PeterDinhBC@gmail.com>
2024-01-30 15:39:25 -0800

    Completed eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,12 +1,3 @@
-// Compares 2 value 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,14 +10,25 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+// Compares 2 value 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (const key of Object.keys(object1)) {
-      if (object2[key]) { // this checks if the key "color" is in object2
-        if (object1[key] === object2[key]) {
+      if (object2[key]) {
+        if (Array.isArray(object1[key], object2[key])) {
+          return eqArrays(object1[key], object2[key]);
+          } else if (object1[key] === object2[key]) {
           return true;
         }
       }
