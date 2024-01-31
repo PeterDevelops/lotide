842be88070ffd19a0a5723cef665a9a92897a787
PeterDevelops <PeterDinhBC@gmail.com>
2024-01-31 13:47:36 -0800

    Array test failed, added eqArray function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,15 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) { // checks lengths of the keys
     for (const key of Object.keys(object1)) {
