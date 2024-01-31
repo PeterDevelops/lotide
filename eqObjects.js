76cad18df603a7362003b820037379d2cbf16fa8
PeterDevelops <PeterDinhBC@gmail.com>
2024-01-31 13:32:50 -0800

    Added spacing in logic for readability

--- eqObjects.js
+++ eqObjects.js
@@ -26,6 +26,7 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) { // checks lengths of the keys
     for (const key of Object.keys(object1)) {
       if (object2[key]) { // checks if object2 has that key property
+
         if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // checks if object1[key], and object2[key] are arrays
             if (!eqArrays(object1[key], object2[key])) {
               return false;
@@ -34,6 +35,7 @@ const eqObjects = function(object1, object2) {
         else if (object1[key] !== object2[key]) {
           return false;
         }
+        
       } else {
         return false; // returns false if object2[key] does not exist in object2
       }
