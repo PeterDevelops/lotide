0dd919cb08d1ff10a7e0bbfab95ac2bc5d716ee4
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-13 17:16:47 -0800

    Deleted unecessary require

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-const assertEqual = require("./assertEqual");
 // An array with only one element should still yield that one element as its head
 // An empty array should yield undefined as its head
 
