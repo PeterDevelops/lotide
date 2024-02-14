7c97d3146ef4cc595a16b47af17e0696b3fa57c3
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-13 18:18:46 -0800

    Committed wrong file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-// Checks if the 2 arrays lengths are the same and compares their primitive values
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 const assertArraysEqual = function(arr1, arr2) {
@@ -19,8 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Returns true 
-assertArraysEqual([1, 2, 3], [1, 2, "3"]); // Returns false
 
-/* For this reason, we will not be writing assertions (test code) for assertArraysEqual. That said, you can and 
-should call the function below its declaration and visually ensure that the message printed to the console is accurate. */
+module.exports = assertArraysEqual;
