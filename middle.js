d14c0d4c6c0be24511e1474ece9dbcf78162fe1d
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-13 18:30:13 -0800

    Fixed middle functions else block

--- middle.js
+++ middle.js
@@ -4,7 +4,7 @@ const middle = function(arr) {
     return [];
   } else if (arr.length % 2 === 0) {
     return [arr[Math.round(arr.length / 2) - 1], arr[Math.round(arr.length / 2)]];
-  } else if (arr.length % 2 !== 0) {
+  } else {
     return [arr[Math.floor(arr.length / 2)]];
   }
 };
