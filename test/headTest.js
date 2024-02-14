b7cea5d027af49061f133dbdd0c63b5acec75142
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-14 14:21:05 -0800

    Added M&C to our lotide file, converted headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,42 @@
+const assert = require('chai').assert;
 const head = require("../head");
-const assertEqual = require("../assertEqual");
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5); // Pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Pass
-assertEqual(head(["Labs"]), "Labs"); // Pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs"); //  Fail
-assertEqual(head([4,6,7]), 5); // Fail
-assertEqual(head([1]), 1); // Pass
-assertEqual(head([]), undefined); // Pass
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  })
+
+  it("returns 'hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello')
+  });
+
+  it("returns '5' for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5)
+  });
+
+  it("returns 'Labs' for ['Labs']", () => {
+    assert.strictEqual(head(["Labs"]), "Labs")
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello')
+  });
+
+  it("returns 4 for [4, 6 ,7]", () => {
+    assert.strictEqual(head([4, 6 ,7]), 4)
+  });
+
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1)
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined)
+  });
+
+});
