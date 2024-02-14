bd817fe56051434a4713f3095f9a047c95c5b8de
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-13 17:27:18 -0800

    Refactored tail.js

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-// // Compares 2 value
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // An array with only one element should yield an empty array for its tail
 // An empty array should yield an empty array for its tail
 
@@ -14,17 +5,4 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case: Array with one or zero element
-assertEqual(tail(["one"]).length, 0); // Should equal to 0
-assertEqual(tail([]).length, 0); // Should be 0
+module.exports = tail;
